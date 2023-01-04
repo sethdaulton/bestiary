31ddd35b549d04800d09f9dc5fd3915caea53b63
slthurman01 <slthurman01@gmail.com>
2023-01-03 19:48:24 -0600

    fixes connection.js file.

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
   const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -16,6 +11,5 @@ if (process.env.JAWSDB_URL) {
       port: 3306,
     }
   );
-}
 
 module.exports = sequelize;
