0ff204816835eead1ef8b08130f9542f687e7d9f
slthurman01 <slthurman01@gmail.com>
2023-01-03 19:52:12 -0600

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,11 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
   const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -11,5 +16,6 @@ require('dotenv').config();
       port: 3306,
     }
   );
+}
 
 module.exports = sequelize;
