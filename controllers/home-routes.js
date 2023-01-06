6c83df48b759dd9d52d37c887fcae0abc1bdea49
slthurman01 <slthurman01@gmail.com>
2023-01-06 10:31:06 -0600

    work on file structure.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,14 +2,6 @@ const  express = require ("express");
 const router = express.Router();
 const {Monster} = require("../models");
 
-//login page
-//
-//
-
-//signup page
-//
-//
-
 //home page 
 router.get("/", function (req, res) {
     Monster.findAll().then((monsterData) => {
@@ -20,8 +12,19 @@ router.get("/", function (req, res) {
     })
 });
 
+//login page
 router.get('/login', (req, res) => {
     res.render("login")
+})
+
+//create monster page
+router.get('/createmonster', (req, res) => {
+    res.render("createmonster")
+})
+
+//signup page
+router.get('/signup', (req, res) => {
+    res.render("signup")
 })
 
 module.exports = router;
